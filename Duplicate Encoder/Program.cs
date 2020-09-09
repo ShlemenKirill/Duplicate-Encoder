9baf54dc38f50b5b9ebea1ef45ef6e553c89d823
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-09-09 11:00:15 +0300

    Добавьте файлы проекта.

---
+++ Duplicate Encoder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Duplicate_Encoder
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            string lower = word.ToLower();
+            char[] a = lower.ToCharArray();
+            List<char> str = new List<char>();
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i].Equals(a[j]))
+                    {
+                        count++;
+                    }
+
+                }
+                if (count > 1)
+                {
+                    str.Add(')');
+
+                }
+
+                if (count < 2)
+                {
+                    str.Add('(');
+
+                }
+                count = 0;
+            }
+
+            var result = String.Join("", str.ToArray());
+            return result;
+        }
+    }    
+}
